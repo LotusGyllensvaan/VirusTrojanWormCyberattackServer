529ccb6118008b8ddc457ce53bdf7b2a5c89bac1
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-10-21 10:13:32 +0200

    First Commit
    Added request class and another test

--- lib/request.rb
+++ lib/request.rb
@@ -1,9 +1,17 @@
 class Request
   
-  attr_reader :method, :resource
+  attr_reader :method, :resource, :version, :headers, :params
 
   def initialize(request_string)
-    @method = :get
-    @resource = "/"
+    @request_lines = request_string.split("\n")
+
+    @attributes = @request_lines.map { |line| line.split(": ")}
+    @header_line = @attributes[0][0].split(" ")
+
+    @method = @header_line[0].downcase.to_sym
+    @resource = @header_line[1]
+    @version = @header_line[2]
+
+    @headers = @attributes.map { |key, value| [key, value] if key && value }.compact.to_h
   end
 end
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -1,22 +1,26 @@
 require_relative 'spec_helper'
 require_relative '../lib/request'
 
+request_string = File.read('./test/example_requests/get-index.request.txt')
+
 describe 'Request' do
 
     describe 'Simple get-request' do
     
         it 'parses the http method' do
-            request_string = File.read('./test/example_requests/get-index.request.txt')
             request = Request.new(request_string)
             _(request.method).must_equal :get
         end
 
         it 'parses the resource' do
-            request_string = File.read('./test/example_requests/get-index.request.txt')
             request = Request.new(request_string)
             _(request.resource).must_equal "/"
         end
 
+        it 'parses the version' do
+            request = Request.new(request_string)
+            _(request.version).must_equal "HTTP/1.1"
+        end
 
     end
 
