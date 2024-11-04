0facb95b3e586f0d4d8a91d1eadd2b8481645d0f
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-11-04 10:17:36 +0100

    Params

--- lib/request.rb
+++ lib/request.rb
@@ -3,15 +3,28 @@ class Request
   attr_reader :method, :resource, :version, :headers, :params
 
   def initialize(request_string)
-    @request_lines = request_string.split("\n")
+    @request_string_lines = request_string.split("\n")
 
-    @attributes = @request_lines.map { |line| line.split(": ")}
-    @header_line = @attributes[0][0].split(" ")
+    @header_elements = @request_string_lines.map { |line| line.split(": ")}
+  
+    @request_line = @header_elements[0][0].split(" ")
 
-    @method = @header_line[0].downcase.to_sym
-    @resource = @header_line[1]
-    @version = @header_line[2]
+    @method = @request_line[0].downcase.to_sym
+    @resource = @request_line[1]
+    @version = @request_line[2]
+    
+    @headers = @header_elements.map { |key, value| [key, value] if key && value }.compact.to_h
 
-    @headers = @attributes.map { |key, value| [key, value] if key && value }.compact.to_h
+
+    #params
+    @query_params = @resource.partition("?").last.split("&")
+    @params = Hash[@query_params.map { |param| param.split("=") }]
+  
+    @params = @header_elements.select { |element| element.to_s.include?("=") }
+
   end
 end
+
+request_string = File.read("get-fruits-with-filter.request.txt")
+request = Request.new(request_string)
+p request.params
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 require_relative '../lib/request'
 
-request_string = File.read('./test/example_requests/get-index.request.txt')
+request_string = File.read('example_requests/get-fruits-with-filter.request.txt')
 
 describe 'Request' do
 
@@ -14,12 +14,23 @@ describe 'Request' do
 
         it 'parses the resource' do
             request = Request.new(request_string)
-            _(request.resource).must_equal "/"
+            _(request.resource).must_equal "/fruits?type=bananas&minrating=4"
         end
 
         it 'parses the version' do
             request = Request.new(request_string)
             _(request.version).must_equal "HTTP/1.1"
+        end
+
+        it 'parses the headers' do
+            correct_headers = {
+                'Host' => 'fruits.com',
+                'User-Agent' => 'ExampleBrowser/1.0',
+                'Accept-Encoding' => 'gzip, deflate',
+                'Accept' => '*/*'
+            }
+            request = Request.new(request_string)
+            _(request.headers).must_equal correct_headers
         end
 
     end
