485ecb8263031fbf51348b870dbebfcbeaf12c08
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-11-11 10:16:16 +0100

    Params

--- lib/request.rb
+++ lib/request.rb
@@ -12,19 +12,21 @@ class Request
     @method = @request_line[0].downcase.to_sym
     @resource = @request_line[1]
     @version = @request_line[2]
-    
+
     @headers = @header_elements.map { |key, value| [key, value] if key && value }.compact.to_h
 
+    #query params
+    
+    @query_params_elements = @resource.partition("?").last.split("&")
+    @query_params = Hash[@query_params_elements.map { |param| param.split("=") }]
 
-    #params
-    @query_params = @resource.partition("?").last.split("&")
-    @params = Hash[@query_params.map { |param| param.split("=") }]
-  
-    @params = @header_elements.select { |element| element.to_s.include?("=") }
-
+    #body params
+    @body_params_line = @request_string_lines.select { |line| line.include?("=")}[0]
+    @body_params = @body_params_line.split("&").map { |pair| pair.split("=") }.to_h
+    p @body_params
   end
 end
 
 request_string = File.read("get-fruits-with-filter.request.txt")
 request = Request.new(request_string)
-p request.params
+#p request.params
