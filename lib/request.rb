ad8cb04da956fc0d8ea7e4a9948214eefcc721ea
LotusGyllensvaan <lotus.gyllensvaan@elev.ga.ntig.se>
2024-11-25 12:46:55 +0100

    update

--- lib/request.rb
+++ lib/request.rb
@@ -5,17 +5,13 @@ class Request
   attr_reader :method, :resource, :version, :headers, :params
 
   def initialize(request_string)
-    @request_string_lines = request_string.split('\n')
-
-    @header_elements = @request_string_lines.map { |line| line.split(': ') }
-
-    @request_line = @header_elements[0][0].split(' ')
-
+    @request_string_lines = request_string.split("\n")
+    @request_line = parse_request_line(@request_string_lines[0])
+    
     @method = @request_line[0].downcase.to_sym
     @resource = @request_line[1]
     @version = @request_line[2]
-    
-    @headers = @header_elements.map { |key, value| [key, value] if key && value }.compact.to_h
+    @headers = parse_headers(@request_string_lines)
 
     params_in_query = @resource.include?('?')
 
@@ -27,5 +23,23 @@ class Request
       params_in_body = !@body_params_line.nil?
       @params = params_in_body ? @body_params_line.split('&').map { |pair| pair.split('=') }.to_h : {}
     end
+
   end
+
+    def parse_request_line(request_line_string)
+      request_line_parsed = request_line_string.split(' ')
+    end
+
+    def parse_headers(request_string_lines)
+      request_string_lines.map.with_index {
+         |line, index| line.split(": ") unless index == 0 
+      }.compact.to_h
+    end
+
+    def parse_query_params(request_line)
+      
+    end
 end
+
+request_string = File.read('get-fruits-with-filter.request.txt')
+request = Request.new(request_string)
